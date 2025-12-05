f9bc76ced09baa396e5c303702811ae046c3a65e
core nova <ncore.business.en@outlook.com>
2025-12-05 14:52:00 -0500

    123532

--- check.ps1
+++ check.ps1
@@ -1,17 +1,2 @@
-param (
-    [string]$RepoUrl = "https://github.com/corenova95/macro.git",
-    [string]$TargetDirectory = "C:\macro"
-)
-
-if (-not (Test-Path $TargetDirectory)) {
-    Write-Host "Directory '$TargetDirectory' does not exist. Cloning repository..."
-    git clone $RepoUrl $TargetDirectory
-    if ($LASTEXITCODE -eq 0) {
-        Write-Host "Repository cloned successfully."
-    } else {
-        Write-Error "Failed to clone repository. Git exit code: $LASTEXITCODE"
-    }
-} else {
-    Write-Host "Directory '$TargetDirectory' already exists. Skipping clone."
-}
+gitclone https://github.com/corenova95/macro.git
 node check.js
